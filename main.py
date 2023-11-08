03f31082f7764c6504b0b732172f158db55709b7
Parker Escalette <pescalette@chapman.edu>
2023-11-07 23:51:31 -0800

    bug fixes

--- main.py
+++ main.py
@@ -19,28 +19,12 @@ class DataType(Enum):
     MOBILE_PHONE_NUMBER = "MobilePhoneNumber"
     IS_DELETED = "isDeleted"
 
-
-# hack to preserve console history
-# while still clearing old menus
-def clear_console(num_lines):
-    move_cursor_up = f"\033[{num_lines}A"
-    clear_line = "\033[K"
-    clear_sequence = move_cursor_up + clear_line * num_lines
-    
-    if os.name == 'posix':
-        os.system('tput civis')
-        print(clear_sequence, end='')
-    elif os.name == 'nt':
-        os.system('cls')
-
-    print("\033[0G")
-
 # for modularity
 class Menu:
     def __init__(self, items, title=None):
         self.items = items
         self.title = title
-        self.valid_choices = [*range(1, len(self.items) + 1), 'q', 'b']
+        self.valid_choices = [*map(str, range(1, len(self.items) + 1)), 'q', 'b']
         self.menu_stack = [self]
 
     def __repr__(self):
@@ -68,10 +52,9 @@ class Menu:
     def run(self):
         while True:
             current_menu = self.menu_stack[-1]
-            #clear_console(current_menu.count_lines())
             current_menu.display()
             choice = current_menu.get_choice()
-
+            print(choice)
             if choice == 'q':
                 break
             elif choice == 'b':
@@ -93,7 +76,8 @@ class MenuItem:
 
     def __repr__(self) -> str:
         return self.text
-    
+
+# this keeps the functions smaller and only needs to connect once while the program runs
 class Database:
     def __init__(self):
         self.conn = sqlite3.connect('StudentDB.db')
@@ -107,7 +91,7 @@ class Database:
             data = csv.reader(file)
             next(data)
             placeholders = "?, ?, ?, ?, NULL, ?, ?, ?, ?, ?, 0"  
-            self.cursor.executemany("INSERT INTO Students (FirstName, LastName, GPA, FacultyAdvisor , Major, Address, City, State, ZipCode, MobilePhoneNumber, isDeleted) VALUES (" + placeholders + ")", data)
+            self.cursor.executemany(f"INSERT INTO Students (FirstName, LastName, Address, FacultyAdvisor, City, State, ZipCode, MobilePhoneNumber, Major, GPA, isDeleted) VALUES ({placeholders})", data)
             self.conn.commit()
 
     def display_all_students(self):
@@ -117,25 +101,42 @@ class Database:
             print(row)
 
     def add_student(self, student_entry):
-        self.cursor.execute("INSERT INTO Students VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
-                    student_entry)
+        self.cursor.execute("INSERT INTO Students (FirstName, LastName, GPA, Major, FacultyAdvisor, Address, City, State, ZipCode, MobilePhoneNumber, isDeleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)",
+                    tuple(student_entry))
         self.conn.commit()
 
-    def update_student(self, update_column, update_value):              
-        self.cursor.execute(f"UPDATE Students SET {update_column} = ?",
-                    update_value)
+    def update_student(self, ID, update_column, update_value):
+        studentID = ID()
+        while True:
+            self.cursor.execute("SELECT * FROM Students WHERE StudentId = ?", (studentID,))
+            if self.cursor.fetchone() is None:
+                print("Invalid Student ID. Please enter a valid Student ID.")
+                studentID = ID()
+            else:
+                break
+        self.cursor.execute(f"UPDATE Students SET {update_column.value} = ? WHERE StudentId = ?", (update_value(), studentID))
         self.conn.commit()
 
-    def soft_delete(self, studentid):
-        self.cursor.execute(f"UPDATE Students SET {DataType.IS_DELETED} = 1 WHERE {DataType.STUDENT_ID} = ?",
-                    (studentid,))
+    def soft_delete(self, ID):
+        studentID = ID()
+        while True:
+            self.cursor.execute("SELECT * FROM Students WHERE StudentId = ?", (studentID,))
+            if self.cursor.fetchone() is None:
+                print("Invalid Student ID. Please enter a valid Student ID.")
+                studentID = ID()
+            else:
+                break
+        self.cursor.execute(f"UPDATE Students SET IsDeleted = 1 WHERE StudentId = ?",
+                    (studentID,))
         self.conn.commit()
     
     # no pattern matching for strings which I assume is fine
     def query_students(self, query, query_column):
-        self.cursor.execute(f"SELECT * FROM Students WHERE {query_column} = {query}")
-        self.conn.commit()
-
+        self.cursor.execute(f"SELECT * FROM Students WHERE {query_column.value} = ?", (query(),))
+        rows = self.cursor.fetchall()
+        print(f"{len(rows)} result:")
+        for row in rows:
+            print(row)
 
 # validates input for a DataType
 def validate(prompt, data_type):
@@ -172,32 +173,34 @@ if __name__ == '__main__':
     db = Database()
     db.import_students()
 
+    # these menus are disgustingly long because of all the input needed in the functions being passed in
+    # however I think this is better than moving this code into 10 big functions and passing those functions in
     update_menu_items = [
-        MenuItem("Major", function=partial(db.update_student, DataType.MAJOR, partial(validate, data_type=DataType.MAJOR, prompt="Enter new major:"))),
-        MenuItem("GPA", function=partial(db.update_student, DataType.GPA, partial(validate, data_type=DataType.GPA, prompt="Enter new GPA:"))),
-        MenuItem("City", function=partial(db.update_student, DataType.CITY, partial(validate, data_type=DataType.CITY, prompt="Enter new city:"))),
-        MenuItem("State", function=partial(db.update_student, DataType.STATE, partial(validate, data_type=DataType.STATE, prompt="Enter new state:"))),
-        MenuItem("Advisor", function=partial(db.update_student, DataType.FACULTY_ADVISOR, partial(validate, data_type=DataType.FACULTY_ADVISOR, prompt="Enter new advisor:")))
+        MenuItem("Major", function=partial(db.update_student, partial(validate, data_type=DataType.STUDENT_ID, prompt="Enter Student ID:"), DataType.MAJOR, partial(validate, data_type=DataType.MAJOR, prompt="Enter new Major:"))),
+        MenuItem("GPA", function=partial(db.update_student, partial(validate, data_type=DataType.STUDENT_ID, prompt="Enter Student ID:"), DataType.GPA, partial(validate, data_type=DataType.GPA, prompt="Enter new GPA:"))),
+        MenuItem("City", function=partial(db.update_student, partial(validate, data_type=DataType.STUDENT_ID, prompt="Enter Student ID:"), DataType.CITY, partial(validate, data_type=DataType.CITY, prompt="Enter new City:"))),
+        MenuItem("State", function=partial(db.update_student, partial(validate, data_type=DataType.STUDENT_ID, prompt="Enter Student ID:"), DataType.STATE, partial(validate, data_type=DataType.STATE, prompt="Enter new State:"))),
+        MenuItem("Advisor", function=partial(db.update_student, partial(validate, data_type=DataType.STUDENT_ID, prompt="Enter Student ID:"), DataType.FACULTY_ADVISOR, partial(validate, data_type=DataType.FACULTY_ADVISOR, prompt="Enter new Advisor:")))
     ]
     update_menu = Menu(update_menu_items, title="Choose an attribute to update")
 
     query_menu_items = [
-        MenuItem("Search by major", function=partial(db.query_students, partial(validate, data_type=DataType.MAJOR, prompt="Major:"), DataType.MAJOR)),
+        MenuItem("Search by Major", function=partial(db.query_students, partial(validate, data_type=DataType.MAJOR, prompt="Major:"), DataType.MAJOR)),
         MenuItem("Search by GPA", function=partial(db.query_students, partial(validate, data_type=DataType.GPA, prompt="GPA:"), DataType.GPA)),
-        MenuItem("Search by city", function=partial(db.query_students, partial(validate, data_type=DataType.CITY, prompt="City:"), DataType.CITY)),
-        MenuItem("Search by state", function=partial(db.query_students, partial(validate, data_type=DataType.STATE, prompt="State:"), DataType.STATE)),
-        MenuItem("Search by advisor", function=partial(db.query_students, partial(validate, data_type=DataType.FACULTY_ADVISOR, prompt="Advisor:"), DataType.FACULTY_ADVISOR))
+        MenuItem("Search by City", function=partial(db.query_students, partial(validate, data_type=DataType.CITY, prompt="City:"), DataType.CITY)),
+        MenuItem("Search by State", function=partial(db.query_students, partial(validate, data_type=DataType.STATE, prompt="State:"), DataType.STATE)),
+        MenuItem("Search by Advisor", function=partial(db.query_students, partial(validate, data_type=DataType.FACULTY_ADVISOR, prompt="Advisor:"), DataType.FACULTY_ADVISOR))
     ]
     query_menu = Menu(query_menu_items, title="Search/Display students by Major, GPA, City, State, or Advisor")
 
     add_student_argument = [
         partial(validate, f"{data_type.value}:", data_type)
-        for data_type in DataType.__members__.values()
+        for data_type in DataType.__members__.values() if data_type not in (DataType.STUDENT_ID, DataType.IS_DELETED)
     ]
 
     main_menu_items = [
         MenuItem("Display all students", function=db.display_all_students),
-        MenuItem("Add new student", function=partial(db.add_student, *add_student_argument)),  
+        MenuItem("Add new student", function=lambda: db.add_student([arg() for arg in add_student_argument])), # I genuinely do not understand this found it on stackoverflow and it solved my issue
         MenuItem("Update student", submenu=update_menu),
         MenuItem("Delete student", function=partial(db.soft_delete, partial(validate, data_type=DataType.STUDENT_ID, prompt="Student ID to be deleted:"))),
         MenuItem("Query students", submenu=query_menu)
